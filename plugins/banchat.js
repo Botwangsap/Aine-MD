ea673ed21e0c1b99584feb4cb9fd50ea90b44e8b
Botwangsap <107746504+Botwangsap@users.noreply.github.com>
2022-06-28 06:47:28 +0700

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, participants }) => {
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('Done!')
+    m.reply('Bot telah di mute,initinya bot ini tidak bisa diakses di grup ini>.<😊!')
   // } else m.reply('Ada nomor host disini...')
 }
 handler.help = ['banchat']
