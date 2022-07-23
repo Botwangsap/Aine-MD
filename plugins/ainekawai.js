f05b0409b121fafca6fa08b3de27d635737c2d34
Botwangsap <107746504+Botwangsap@users.noreply.github.com>
2022-07-24 06:51:27 +0700

    Update ainekawai.js

--- plugins/ainekawai.js
+++ plugins/ainekawai.js
@@ -1,16 +1,16 @@
 let fs = require('fs')
 let handler = async (m) => {
-let helloaine = fs.readFileSync('./mp3/kawai.opus')
+let helloaine = fs.readFileSync('./mp3/AUD-20220329-WA0662.opus')
 conn.sendFile(m.chat, helloaine, '', '', m, true)
 }
 
-handler.customPrefix = /^(kawai|kawaii)$/i
+handler.customPrefix = /^(Kawai|Kawaii|Cu|ahhh|Yameteh|💦|Masukin ayang|Ahh|Crott💦|)$/i
 handler.command = new RegExp
 
 handler.limit = true
 handler.mods = false 
 handler.premium = false
-handler.group = true
+handler.group = false
 handler.private = false
 
 module.exports = handler
