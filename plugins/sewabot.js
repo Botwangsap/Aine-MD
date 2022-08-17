4579cb600b9f7a49e17a1c921c08c62ea48782ba
Botwangsap <107746504+Botwangsap@users.noreply.github.com>
2022-08-17 09:03:00 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
   let pepe = 'https://telegra.ph/file/2e2eb7d093fec08fd00d4.jpg'
   let baper = await fetch(pepe).then(a => a.buffer())
 
-  let aine = '6289654360447@s.whatsapp.net'
+  let aine = '6281998425782@s.whatsapp.net'
   let a = await conn.profilePictureUrl(conn.user.jid, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
   let b = await conn.profilePictureUrl(owner[0]+'@s.whatsapp.net', 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
   let c = pickRandom([a, b])
@@ -30,8 +30,8 @@ wa.me/${owner[0]}
   productListInfo: {
   productSections: [{
   title:'Klik untuk order',
-  products:[{productId:'5522349281155709'}]}],
-  headerImage: { productId: '5522349281155709',
+  products:[{productId:'5793781197321892'}]}],
+  headerImage: { productId: '5793781197321892',
   jpegThumbnail: baper },
   businessOwnerJid: `6281998425782@s.whatsapp.net`
   },
